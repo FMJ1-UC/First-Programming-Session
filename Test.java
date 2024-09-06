1f0a0fb2e6aa6c3d29d77aca53e457d0a29b658f
FMJ1-UC <cbf6520@students.uc-bcf.edu.ph>
2024-09-06 08:46:58 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+/**
+ *
+ * @author m303user
+ */ 
+/*september 6, 2024
+ANNA
+FIRST PROGRAMMING SESSION*/
+public class Test {
+    public static void main(String[]args){
+        //Java Escape characters 
+        System.out.print(" ANNA \t\tRHODORA \nQUINTALEG");
+        System.out.println(" \n\n\nAnna\"s Class ");
+        System.out.print(" I LOVE JAVA PROGRAMMING ");
+    }
+}
